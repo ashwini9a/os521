b4a060d21a1810ef0620256ec6263fd78e3e3ccb
yash <yivijay@gmail.com>
2016-03-02 20:38:35 -0500

    changed location of fidescriptor_init()

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -49,65 +49,72 @@ int filedescriptor_init(void) {
 	
 	char *filename;
 	filename = kstrdup("con:");
+	char *out = kstrdup("con:");
+	char *err = kstrdup("con:");
 	struct vnode *vn1;
 	struct vnode *vn2;
 	struct vnode *vn3;
 
-	curproc->filedescriptor[0] =(struct filehandle*) kmalloc(sizeof(struct filehandle*));
 	if (curproc->filedescriptor[0] == NULL) {
-		kprintf("could not allocate memory for std in");
-		return ENFILE;
-	}
+		curproc->filedescriptor[0] =(struct filehandle*) kmalloc(sizeof(struct filehandle));
+		if (curproc->filedescriptor[0] == NULL) {
+			kprintf("could not allocate memory for std in");
+			return ENFILE;
+		}
 	
-	int result = vfs_open(filename, O_RDONLY, 0664, &vn1);
-	if (result) {
-		kprintf("Console 0 open failed");
-		return EINVAL;
+		int result = vfs_open(filename, O_RDONLY, 0664, &vn1);
+		if (result) {
+			kprintf("Console 0 open failed");
+			return EINVAL;
+		}
+
+		curproc->filedescriptor[0]->filename = filename;
+		curproc->filedescriptor[0]->flags = O_RDONLY;
+		curproc->filedescriptor[0]->offset = 0;
+		curproc->filedescriptor[0]->refcount = 1;
+		curproc->filedescriptor[0]->filelock = lock_create("stdin_lock");
+		curproc->filedescriptor[0]->vnode = vn1; 
 	}
 
-	curproc->filedescriptor[0]->filename = filename;
-	curproc->filedescriptor[0]->flags = O_RDONLY;
-	curproc->filedescriptor[0]->offset = 0;
-	curproc->filedescriptor[0]->refcount = 1;
-	curproc->filedescriptor[0]->filelock = lock_create("stdin_lock");
-	curproc->filedescriptor[0]->vnode = vn1; 
-
-	curproc->filedescriptor[1] = (struct filehandle*) kmalloc(sizeof(struct filehandle*));
 	if (curproc->filedescriptor[1] == NULL) {
-		kprintf("Could not allocate memory for std out");
-		return ENFILE;
+		curproc->filedescriptor[1] = (struct filehandle*) kmalloc(sizeof(struct filehandle));
+		if (curproc->filedescriptor[1] == NULL) {
+			kprintf("Could not allocate memory for std out");
+			return ENFILE;
+		}
+		int result = vfs_open(out, O_WRONLY, 0664, &vn2);
+		if (result) {
+			kprintf("could not open std output");
+			return EINVAL;
+		}
+
+		curproc->filedescriptor[1]->filename = out;
+		curproc->filedescriptor[1]->flags = O_WRONLY;
+		curproc->filedescriptor[1]->offset = 0;
+		curproc->filedescriptor[1]->refcount = 1;
+		curproc->filedescriptor[1]->filelock = lock_create("stdout_lock");
+		curproc->filedescriptor[1]->vnode = vn2;
 	}
-	result = vfs_open(filename, O_WRONLY, 0664, &vn2);
-	if (result) {
-		kprintf("could not open std output");
-		return EINVAL;
-	}
 
-	curproc->filedescriptor[1]->filename = filename;
-	curproc->filedescriptor[1]->flags = O_WRONLY;
-	curproc->filedescriptor[1]->offset = 0;
-	curproc->filedescriptor[1]->refcount = 1;
-	curproc->filedescriptor[1]->filelock = lock_create("stdout_lock");
-	curproc->filedescriptor[1]->vnode = vn2;
-
-
-	curproc->filedescriptor[2] = (struct filehandle*) kmalloc(sizeof(struct filehandle*));
 	if (curproc->filedescriptor[2] == NULL) {
-		kprintf("Could not allocate memory for std err");
-		return ENFILE;
-	}
-	result = vfs_open(filename, O_WRONLY, 0664, &vn3);
-	if (result) {
-		kprintf("could not open std err");
-		return EINVAL;
-	}
+		curproc->filedescriptor[2] = (struct filehandle*) kmalloc(sizeof(struct filehandle));
+		if (curproc->filedescriptor[2] == NULL) {
+			kprintf("Could not allocate memory for std err");
+			return ENFILE;
+		}
+		int result = vfs_open(err, O_WRONLY, 0664, &vn3);
+		if (result) {
+			kprintf("could not open std err");
+			return EINVAL;
+		}
 
-	curproc->filedescriptor[2]->filename = filename;
-	curproc->filedescriptor[2]->flags = O_WRONLY;
-	curproc->filedescriptor[2]->offset = 0;
-	curproc->filedescriptor[2]->refcount = 1;
-	curproc->filedescriptor[2]->filelock = lock_create("stderr_lock");
-	curproc->filedescriptor[2]->vnode = vn2;
+		curproc->filedescriptor[2]->filename = err;
+		curproc->filedescriptor[2]->flags = O_WRONLY;
+		curproc->filedescriptor[2]->offset = 0;
+		curproc->filedescriptor[2]->refcount = 1;
+		curproc->filedescriptor[2]->filelock = lock_create("stderr_lock");
+		curproc->filedescriptor[2]->vnode = vn2;
+	}
 
 	return 0;
 }	
